b32fdba3d6c7c6068c6161530edd1da8f7921df1
geison <geisonbruno0@gmail.com>
2023-11-28 15:04:51 -0300

    Estruturas de pacotes

---
+++ src/main/java/com/java/SpringbootRestfullWebServices/SpringbootRestfullWebServicesApplication.java
@@ -0,0 +1,13 @@
+package com.java.SpringbootRestfullWebServices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRestfullWebServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRestfullWebServicesApplication.class, args);
+	}
+
+}
